910dee87dca6ca4944f3ceb1f8bf6c61abf84256
mactavissh <mactavissh@users.noreply.github.com>
2016-01-23 20:16:38 -0600

    Create apod.sh

---
+++ apod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+curl -s http://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss > apod.txt
+line=$(grep "enclosure url=" apod.txt | head -1)
+#<enclosure url="http://www.nasa.gov/sites/default/files/thumbnails/image/pia06654.jpg" length="97237" type="image/jpeg" />
+echo $line
+
+if [ ! -z "$line" ]
+then
+        url="`echo $line | awk -F'[\"]' '{print $2}'`"
+        echo $url
+        curl $url > apod.jpg
+        gsettings set org.gnome.desktop.background picture-uri "file:///home/rlm/apod_bash/apod.jpg"
+else
+                echo "Line is empty"
+fi
